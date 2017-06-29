7b4a834230e63ab962dc41b0250d9644253b2a0e
jatgov007 <jatgov007@gmail.com>
2017-06-29 12:57:16 -0400

    prefinal

--- index.php
+++ index.php
@@ -31,7 +31,11 @@
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-
+    <style>
+        #portfolio div.row {
+            padding: 0px 20px 0px 60px;
+        }
+    </style>
 </head>
 
 <body id="page-top">
